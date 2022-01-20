7f474fbac149233d5d5b15aa1057675b5e4fb60d
dhairyahetal <hetallangalia@gmail.com>
2022-01-20 12:57:17 -0500

    c

--- test/AtataSampleApp.UITests/SetUpFixture.cs
+++ test/AtataSampleApp.UITests/SetUpFixture.cs
@@ -12,8 +12,24 @@ namespace AtataSampleApp.UITests
             AtataContext.GlobalConfiguration
                 .ApplyJsonConfig<AtataConfig>()
                 .UseDefaultArtifactsPathIncludingBuildStart(
-                    TestContext.Parameters.Get("UseDefaultArtifactsPathIncludingBuildStart", true))
-                .AutoSetUpDriverToUse();
+                    TestContext.Parameters.Get("UseDefaultArtifactsPathIncludingBuildStart", true));
+               
+         AtataContext.Build().
+    UseDriver(() =>
+    {
+        string driverType = ConfigurationManager.AppSettings["DriverType"];
+        switch (driverType)
+        {
+            case "Firefox":
+                return new FirefoxDriver();
+            case "Chrome":
+                return new ChromeDriver();
+            case "InternetExplorer":
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidOperationException($"Unknown '{driverType}' driver type.");
+        }
+    })
         }
     }
 }
